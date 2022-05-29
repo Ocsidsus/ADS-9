422c502ba83b202566bf36af3d582a79f20eba6d
Dmitry Volkov <83473625+Ocsidsus@users.noreply.github.com>
2022-05-29 13:19:47 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,30 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+    std::ifstream file(filename);
+    BST<std::string> tree;
+    if (!file) {
+        std::cout << "File error!" << std::endl;
+        return tree;
+    }
+    std::string cache = "";
+    while (!file.eof())
+    {
+        int ch = file.get();
+        if (ch >= 'a' && ch <= 'z') {
+            cache += char(ch);
+        } else if (ch >= 'A' && ch <= 'Z') {
+            cache += char(ch + ('a' - 'A'));
+        } else {
+            if (cache != "") {
+                tree.add(cache);
+            }
+            cache.clear();
+        }
+    }
+    if (cache != "") {
+        tree.add(cache);
+    }
+    file.close();
+    return tree;
 }
